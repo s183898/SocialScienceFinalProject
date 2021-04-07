44c764189e59b0db2a9c1b437ebdb9b679db627d
s183898 <s183898@student.dtu.dk>
2021-04-07 11:31:39 +0200

    forecast

--- soup.py
+++ soup.py
@@ -4,4 +4,10 @@ page = requests.get("https://gameofthrones.fandom.com/wiki")
 
 soup = BeautifulSoup(page.content, 'html.parser')
 
-print(list(soup.children))
+print(list(soup.children)[2].get_text())
+
+
+episode = soup.find(id="seven-day-forecast")
+forecast_items = seven_day.find_all(class_="tombstone-container")
+tonight = forecast_items[0]
+print(tonight.prettify())
