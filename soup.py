787694b3fd23d2bab94f4d9638ecb0e21a99a1ad
s183898 <s183898@student.dtu.dk>
2021-04-14 11:16:48 +0200

    nofing new

--- soup.py
+++ soup.py
@@ -1,11 +1,21 @@
 import requests
 from bs4 import BeautifulSoup
-page = requests.get("https://gameofthrones.fandom.com/wiki")
+import json
 
-soup = BeautifulSoup(page.content, 'html.parser')
+url = 'https://gameofthrones.fandom.com/wiki/Home'
+res = requests.get(url)
+html_page = res.content
+soup = BeautifulSoup(html_page, 'html.parser')
+text = soup.find_all(text=True)
 
-print(list(soup.children)[2].get_text())
 
-episode = soup.find(id="seven-day-forecast")
+print(soup.find(["h3"]).next_siblings)
 
-print("GI")
+target = soup.find('h2',text='Topic 2')
+for sib in target.find_next_siblings():
+    if sib.name=="h2":
+        break
+    else:
+        print(sib.text)
+
+
